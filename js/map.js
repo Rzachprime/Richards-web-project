36e8d08167d0313909d9f7861e1483ffe1d15559
Rzachprime <rzachprime@gmail.com>
2016-03-23 04:04:54 -0400

    functioning javascript game
    Finished the javascript game in its basic form. lots of changes to the
    code.

--- js/map.js
+++ js/map.js
@@ -14,11 +14,16 @@ function initMap(){
 
 // Var to hold information from text boxes
 
-var value1 = document.getElementById("latitude").value;
-var value2 = document.getElementById("longitude").value;
+var value1;
+var value2;
 
+// var to hold distance.
+
+var value3;
+
+// var to hold target latitude (target1) and longitude (target3)
 var target1;
-
+var target3;
 
 //var to hold random numbers for dice mechanics. Generates a number range of 1 through 10
 var dice1;
@@ -32,36 +37,47 @@ function diceFate () {
 
 function returnTarget() {
     diceFate ();
+    
 
     if (dice1 == 1) {
-        target1 = "";
+        target1 = "38.0297";
+        target3 = "-84.4947";
         document.getElementById("target2").innerHTML = "Paragraph changed! 1";
     } else if (dice1 == 2) {
-        target1 = "";
+        target1 = "38.0297";
+        target3 = "-84.4947";
         document.getElementById("target2").innerHTML = "Paragraph changed! 2";
     } else if (dice1 == 3) {
-        target1 = "";
+        target1 = "38.0297";
+        target3 = "-84.4947";
         document.getElementById("target2").innerHTML = "Paragraph changed! 3";
     } else if (dice1 == 4) {
-        target1 = "";
+        target1 = "38.0297";
+        target3 = "-84.4947";
         document.getElementById("target2").innerHTML = "Paragraph changed! 4";
     } else if (dice1 == 5) {
-        target1 = "";
+        target1 = "38.0297";
+        target3 = "-84.4947";
         document.getElementById("target2").innerHTML = "Paragraph changed! 5";
     } else if (dice1 == 6) {
-        target1 = "";
+        target1 = "38.0297";
+        target3 = "-84.4947";
         document.getElementById("target2").innerHTML = "Paragraph changed! 6";
     } else if (dice1 == 7) {
-        target1 = "";
+        target1 = "38.0297";
+        target3 = "-84.4947";
         document.getElementById("target2").innerHTML = "Paragraph changed! 7";
     } else if (dice1 == 8) {
-        target1 = "";
+        target1 = "38.0297";
+        target3 = "-84.4947";
         document.getElementById("target2").innerHTML = "Paragraph changed! 8";
     } else if (dice1 == 9) {
-        target1 = "";
+        target1 = "38.0297";
+        target3 = "-84.4947";
         document.getElementById("target2").innerHTML = "Paragraph changed! 9";
     } else {
-        target1 = "";
+        target1 = "38.0297";
+        target3 = "-84.4947";
         document.getElementById("target2").innerHTML = "Paragraph changed! 10";
         
     }
@@ -73,8 +89,44 @@ function returnTarget() {
 
 
 
-// Function to send values to map 
 
+
+// Function to send values to map and generate the result
+function finalResult() {
+    //value3 = "lat: " + value1 + ", lng: " + value2 + ";";
+    
+//    value1 = (isNumber(value1) ? 0 : value1);
+//    value2 = (isNumber(value2) ? 0 : value2);
+//    
+//    value1 = latRange(value1);
+//    value2 = lngRange(value2);
+//    
+//    document.getElementById("latitude").value = value1;
+//    document.getElementById("longitude").value = value2;
+    
+   value1 = document.getElementById("latitude").value;
+   value2 = document.getElementById("longitude").value;
+    
+    
+    
+    
+    initMap();
+    winLose();
+    
+    
+}
+
+//function isNumber(n) {
+//    return !isNaN(parseFloat(n)) && isFinite(n);
+//}
+//
+//function latRange(n) {
+//    return Math.min(Math.max(parseInt(n), -90), 90);
+//}
+//
+//function lngRange(n) {
+//    return Math.min(Math.max(parseInt(n), -180), 180);
+//}
 
 
 // Function to display map and generate the distance result
@@ -83,10 +135,10 @@ function initMap() {
   var bounds = new google.maps.LatLngBounds;
   var markersArray = [];
 
-  var origin1 = {lat: 55.93, lng: -3.118};
-  var origin2 = 'Greenwich, England';
-  var destinationA = 'Stockholm, Sweden';
-  var destinationB = {lat: 50.087, lng: 14.421};
+  var origin1 = new google.maps.LatLng(value1, value2);
+  // var origin2 = 'Greenwich, England';
+  var destinationA = new google.maps.LatLng(target1, target3);
+  // var destinationB = {lat: 50.087, lng: 14.421};
 
   var destinationIcon = 'https://chart.googleapis.com/chart?' +
       'chst=d_map_pin_letter&chld=D|FF0000|000000';
@@ -100,8 +152,8 @@ function initMap() {
 
   var service = new google.maps.DistanceMatrixService;
   service.getDistanceMatrix({
-    origins: [origin1, origin2],
-    destinations: [destinationA, destinationB],
+    origins: [origin1],
+    destinations: [destinationA],
     travelMode: google.maps.TravelMode.DRIVING,
     unitSystem: google.maps.UnitSystem.IMPERIAL,
     avoidHighways: false,
@@ -112,7 +164,7 @@ function initMap() {
     } else {
       var originList = response.originAddresses;
       var destinationList = response.destinationAddresses;
-      var outputDiv = document.getElementById('output');
+       var outputDiv = document.getElementById('output');
       outputDiv.innerHTML = '';
       deleteMarkers(markersArray);
 
@@ -139,14 +191,38 @@ function initMap() {
         for (var j = 0; j < results.length; j++) {
           geocoder.geocode({'address': destinationList[j]},
               showGeocodedAddressOnMap(true));
-          outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+           outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
               ': ' + results[j].distance.text + ' in ' +
               results[j].duration.text + '<br>';
+            value3 = results[j].distance.text;
         }
+          
+          
       }
+    
     }
+      
+      
+      
+          
+          
+      
   });
 }
+
+function winLose() {
+          var results1 = value3;
+          var results2 = parseInt(results1);
+          if (results2 <= 250) {
+              document.getElementById("results3").innerHTML = "YOU WIN! The target has been found."
+          } else {
+              
+              document.getElementById("results3").innerHTML = "SORRY, YOU LOSE! The target got away this time."
+              
+          }
+}
+    
+    
 
 function deleteMarkers(markersArray) {
   for (var i = 0; i < markersArray.length; i++) {
