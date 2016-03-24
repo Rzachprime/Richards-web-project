41a11291b977f2b0f12b9f3ff51ff7b946af5f44
Rzachprime <rzachprime@gmail.com>
2016-03-24 01:41:49 -0400

    angular web app for errors
    added angular error app with email submission

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
- angular.module('errorForm', []);
+ //var app = angular.module('errorForm', []);
+
+
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.showMe = false;
+    $scope.myFunc = function() {
+        $scope.showMe = !$scope.showMe;
+    }
+});
