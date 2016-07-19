6b076528fd33f6c92ded56366213c07d10d38005
Rzachprime <rzachprime@gmail.com>
2016-07-19 18:17:57 -0400

    adding php to project

---
+++ tracking.php
@@ -0,0 +1,36 @@
+<?php
+
+
+$pageTitle = "Error Tracking";
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="Home Page" content="Main page which describes the purpose and general structure">
+
+    
+
+    
+
+
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+<link rel="stylesheet" href="css/normailize.css">
+<link rel="stylesheet" href="css/style.css">
+
+
+
+
+
+
+
+  
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="css/layouts/side-menu-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="css/layouts/side-menu.css">
+    <!--<![endif]-->
+  
